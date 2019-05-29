ecbe308cbe283b30d22171d76d7356600369013a
bdushimi <bernarddushimimana@Bernards-MacBook-Pro.local>
2019-05-29 17:18:55 +0200

    initital boilerplate

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h3>Page Not Found</h3>');
+})
+
+const server = http.createServer(app);
+
+server.listen(3000)
